48133542b79777c861bb1e5b8c633596466144dc
Mohamad Abdul Basir <tagihanbulanan75@gmail.com>
2019-11-22 21:06:07 +0700

    menyalin isi folder build

---
+++ precache-manifest.cd304f40aa2012bfa07178bc47f30c8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1beb4b67ad2c631189b4f1167f3a24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a149be9244ab62c8dd0",
+    "url": "/static/css/main.769e9af4.chunk.css"
+  },
+  {
+    "revision": "4faea8e1140786b828d5",
+    "url": "/static/js/2.654c3474.chunk.js"
+  },
+  {
+    "revision": "7a149be9244ab62c8dd0",
+    "url": "/static/js/main.1066453d.chunk.js"
+  },
+  {
+    "revision": "213c9aa53bf012f9ea5c",
+    "url": "/static/js/runtime-main.8fb0e091.js"
+  }
+]);
